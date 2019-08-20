370ee84ac74c1088800fe50e1b42ff22ba27d7c6
Steve Holden <steve@holdenweb.com>
2019-08-20 22:48:58 +0100

    Replace __new__ with a function.

--- src/hw/object_dict.py
+++ src/hw/object_dict.py
@@ -1,30 +1,10 @@
+sentinel = object()  # Unique instance that cannot be passed as an argument
+
+
 class ObjectDict(dict):
     """
     Makes a dictionary behave like an object, with attribute-style access.
     """
-
-    sentinel = object()  # Unique instance that cannot be passed as an argument
-
-    def __new__(cls, arg=sentinel):
-        """
-        This recursive dict subclass converts a dict into an ObjectDict,
-        which can use attribute access to retrieve and set keys.
-        Floats, ints, strings and existing ObjectDicts are simply
-        returned. Lists are transformed into lists of ObjectDicts.
-        The __init__ method will only be called for ObjectDicts,
-        since only then is the argument class is the same as the returned value.
-        When a dict is passed in, standard dict creation is called.
-        """
-        if arg is cls.sentinel:
-            return super().__new__()
-        elif isinstance(arg, (int, float, str, ObjectDict)):
-            return arg
-        elif isinstance(arg, list):
-            return list(ObjectDict(x) for x in arg)
-        elif isinstance(arg, dict):
-            return super().__new__(cls, arg)
-        else:
-            raise ValueError(f"{type(arg)} objects cannot be ObjectDicts")
 
     def __init__(self, arg=sentinel):
         """
@@ -32,12 +12,10 @@ class ObjectDict(dict):
         instance. The __init__ method actually initialises the values
         in the underlying dict.
         """
-        if type(arg) is ObjectDict:
-            return  # Assume existing ObjectDicts do not need re-initialising
-        if arg is self.sentinel:  # Called without args
+        if arg is sentinel:  # Called without args
             arg = {}
         for (k, v) in arg.items():
-            self[k] = ObjectDict(v)
+            self[k] = od_type(v)
 
     def __getattr__(self, name: str):
         try:
@@ -46,8 +24,30 @@ class ObjectDict(dict):
             raise AttributeError(name)
 
     def __setattr__(self, name, value):
-        value = ObjectDict(value)
+        value = od_type(value)
         super().__setitem__(name, value)
 
     def __dir__(self):
         return sorted(self.keys)
+
+
+def od_type(arg=sentinel):
+    """
+    This recursive dict subclass converts a dict into an ObjectDict,
+    which can use attribute access to retrieve and set keys.
+    Floats, ints, strings and existing ObjectDicts are simply
+    returned. Lists are transformed into lists of ObjectDicts.
+    The __init__ method will only be called for ObjectDicts,
+    since only then is the argument class is the same as the returned value.
+    When a dict is passed in, standard dict creation is called.
+    """
+    if arg is sentinel:
+        return ObjectDict()
+    elif isinstance(arg, (int, float, str, ObjectDict)):
+        return arg
+    elif isinstance(arg, list):
+        return list(od_type(x) for x in arg)
+    elif isinstance(arg, dict):
+        return ObjectDict(arg)
+    else:
+        raise ValueError(f"{type(arg)} objects cannot be ObjectDicts")
